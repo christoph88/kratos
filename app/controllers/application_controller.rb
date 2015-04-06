0399d883f1e28dd7a6883ed9c0037d27fa2eb878
Christoph Geypen <geypenchristoph@gmail.com>
2015-04-06 20:45:34 +0000

    get translation working with wookie file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :set_user_language, :configure_permitted_parameters, if: :devise_controller? 
+  before_action :configure_permitted_parameters, if: :devise_controller? 
+  before_action :set_locale 
+
 
   protected
 
@@ -16,9 +18,9 @@ class ApplicationController < ActionController::Base
 
 
   private
-
-  def set_user_language
-    I18n.locale = 'wk'
+ 
+  def set_locale
+    I18n.locale = :wk || I18n.default_locale
   end
   
   #-> Prelang (user_login:devise)
