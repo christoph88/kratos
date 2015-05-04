f002c5249670bfb4325660f37b000ec832949eda
Christoph Geypen <geypenchristoph@gmail.com>
2015-05-04 21:24:18 +0200

    fix rspec test for landingpage

--- spec/controllers/landings_controller_spec.rb
+++ spec/controllers/landings_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe LandingsController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
-      get :index
+      get :landingpage
       expect(response).to have_http_status(:success)
     end
   end
