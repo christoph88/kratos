53822cb70cbf3e8f4b23e9983169ce05a6414484
Christoph Geypen <geypenchristoph@gmail.com>
2015-08-22 21:11:11 +0200

    set robots for each environment

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,4 +1,9 @@
 class LandingsController < ApplicationController
+  def robots                                                                                                                                      
+    robots = File.read(Rails.root + "config/robots/#{Rails.env}.txt")
+    render :text => robots, :layout => false, :content_type => "text/plain"
+  end
+
   def index
     @title = t('landings.index.title')
     set_meta_tags keywords:     %w[leaderboards contests weightlifting powerlifting strongman compete ranking],
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get '/robots.txt' => 'home#robots'
+
   shallow do
     resources :contests do
       resources :submissions, except: :edit
