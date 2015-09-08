f90c0033a69b8925c5d3ce7f4b136f59578a784e
Christoph Geypen <geypenchristoph@gmail.com>
2015-09-08 19:38:07 +0200

    removed pounds from submissions

--- db/migrate/20150813170016_add_pounds_to_submissions.rb
+++
@@ -1,8 +0,0 @@
-class AddPoundsToSubmissions < ActiveRecord::Migration
-  def up
-    add_column :submissions, :pounds, :boolean
-  end
-  def down
-    remove_column :submissions, :pounds, :boolean
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20150906170131) do
     t.integer  "contest_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "pounds"
     t.decimal  "tonnage"
   end
 
