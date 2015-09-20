7094d3d6c1801ac422560d5eb19c1fdce03d2157
Christoph Geypen <geypenchristoph@gmail.com>
2015-09-20 21:29:30 +0200

    created feature tests for homepage

--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -1,8 +1,28 @@
 require "test_helper"
 
 feature "CanAccessHome" do
-  scenario "the test is sound" do
+  scenario "Has branding" do
     visit root_path
-    page.must_have_content "Hello World"
+    page.must_have_content "Kratos"
+    page.find(".navbar-brand")
   end
+
+  scenario "Has a header and a footer" do
+    visit root_path
+    page.has_css?(".header-v6").must_equal true
+    page.has_css?(".footer-v2").must_equal true
+  end
+
+  scenario "Has correct header links" do
+  end
+
+  scenario "Has correct footer links" do
+  end
+
+  scenario "Has twitter in the footer" do
+  end
+
+  scenario "Has a dynamic menu" do
+  end
+
 end
