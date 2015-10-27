4c4aeee81afac7828b575823275bd5b5d9779ee5
Christoph Geypen <geypenchristoph@gmail.com>
2015-10-27 22:42:16 +0100

    added optimize tasks to contest features

--- test/features/can_create_contests_test.rb
+++ test/features/can_create_contests_test.rb
@@ -3,9 +3,14 @@ require "test_helper"
 feature "CanCreateContests" do
   scenario "Can create leaderboards after logging in" do
     
-    #FIXME create a leaderboard and login
-    visit new_user_session_path
-    page.must_have_content "Login to your account"
+    visit root_path
+    within ".navbar-nav" do
+      click_link "leaderboards"
+    end
+
+    page.must_have_content "Show all leaderboards"
+    page.has_css?(".grid-boxes-caption").must_equal true
+    click_link "Create leaderboard"
 
     within ".reg-page" do
       fill_in "user[login]", with: "chris88"
@@ -14,7 +19,22 @@ feature "CanCreateContests" do
     end
 
     page.must_have_content "Create"
-    save_and_open_page
+
+    within ".reg-page" do
+      fill_in "contest[name]", with: "contestname"
+      fill_in "contest[description]", with:"testdescription"
+      find('input[name="commit"]').click
+    end
+
+    page.must_have_content "contestname"
+    
+    #OPTIMIZE must placeholder when no submission with call to action
+    page.must_have_content ".placeholder"
+    #OPTIMIZE must have edit button
+    page.must_have_content "edit"
+    #OPTIMIZE must have all translations
+    page.wont_have_content "change me"
+    #save_and_open_page
 
   end
 end
