34fc616481fc6a02f4fa686c225b1c72769c3f24
Christoph Geypen <geypenchristoph@gmail.com>
2015-11-05 15:43:29 +0100

    fix can update and destroy contests feature test

--- test/features/can_update_and_destroy_contests_test.rb
+++ test/features/can_update_and_destroy_contests_test.rb
@@ -2,24 +2,41 @@ require "test_helper"
 
 feature "CanUpdateAndDestroyContests" do
   scenario "Can update or destroy leaderboards after logging in" do
-    
-    visit root_path
-    within ".navbar-nav" do
-      click_link "leaderboards"
-    end
 
-    page.must_have_content "Show all leaderboards"
-    page.has_css?(".grid-boxes-caption").must_equal true
-    click_link "contestsuperior"
+    #first login because edit contest relies on current_user
 
-    # find a contest without any submission, or else the placeholder does not show.
-    find('div.container.content.profile > div > div.col-md-3.md-margin-bottom-40 > p:nth-child(4) > a').click
+    visit new_user_session_path
 
     within ".reg-page" do
       fill_in "user[login]", with: "chris88"
       fill_in "user[password]", with:"testtest"
       find('input[name="commit"]').click
     end
+
+    #proceed to root to start testing properly
+    
+    visit root_path
+
+    within ".navbar-nav" do
+      click_link "leaderboards"
+    end
+
+    page.must_have_content "Show all leaderboards"
+
+    page.has_css?(".grid-boxes-caption").must_equal true
+    
+    # placeholder only on pages without any submissions
+
+    click_link "contest"
+    save_and_open_page
+    page.has_css? ".placeholder"
+
+    # look for contests with submissions
+    visit contests_path
+
+    click_link "contestsuperior"
+
+    click_link "Edit this contest"
 
     page.must_have_content "Edit leaderboard"
 
@@ -30,9 +47,6 @@ feature "CanUpdateAndDestroyContests" do
     end
 
     page.must_have_content "editcontest"
-    
-    #only on pages without any submissions
-    page.wont_have_content ".placeholder"
 
     visit contests_path
     page.must_have_content "editcontest"
