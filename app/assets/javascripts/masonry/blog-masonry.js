6a9fa8f5d306652c1f142693a02eebb439cf22a1
Christoph Geypen <geypenchristoph@gmail.com>
2015-11-11 12:25:15 +0100

    experimenting with masonry

--- app/assets/javascripts/masonry/blog-masonry.js
+++ app/assets/javascripts/masonry/blog-masonry.js
@@ -5,28 +5,35 @@
 $(document).ready(function(){
     var $container = $('.grid-boxes');
 
-    var gutter = 30;
-    var min_width = 300;
+    //var gutter = 30;
+    //var min_width = 300;
     $container.imagesLoaded( function(){
         $container.masonry({
             itemSelector : '.grid-boxes-in',
-            gutterWidth: gutter,
+            gutterWidth: '.grid-boxes-gutter',
             isAnimated: true,
-              columnWidth: function( containerWidth ) {
-                var box_width = (((containerWidth - 2*gutter)/3) | 0) ;
+            columnWidth: '.grid-boxes-in'
+          
+          //function( containerWidth ) {
+                //var box_width = (((containerWidth - 2*gutter)/3) | 0) ;
+                // (1000 - 60)/3 = 940/3 = 313
+                // (x-60)/3 = 300
+                // x-60 = 300*3 = 900
+                // x = 960
 
-                if (box_width < min_width) {
-                    box_width = (((containerWidth - gutter)/2) | 0);
-                }
+                //if (box_width < min_width) {
+                    //box_width = (((containerWidth - gutter)/2) | 0);
+                    // 960-30 /2 = 960/2 = 480
+                //}
 
-                if (box_width < min_width) {
-                    box_width = containerWidth;
-                }
+                //if (box_width < min_width) {
+                    //box_width = containerWidth;
+                //}
 
-                $('.grid-boxes-in').width(box_width);
+                //$('.grid-boxes-in').width(box_width);
 
-                return box_width;
-              }
+                //return box_width;
+              //}
         });
     });
 });
