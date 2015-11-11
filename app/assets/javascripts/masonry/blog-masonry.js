8211a030f1e46f2912d2dcb75808b6813c1d491d
Christoph Geypen <geypenchristoph@gmail.com>
2015-11-11 13:08:14 +0100

    cleanup masonry code

--- app/assets/javascripts/masonry/blog-masonry.js
+++ app/assets/javascripts/masonry/blog-masonry.js
@@ -5,36 +5,12 @@
 $(document).ready(function(){
     var $container = $('.grid-boxes');
 
-    //var gutter = 30;
-    //var min_width = 300;
     $container.imagesLoaded( function(){
         $container.masonry({
             itemSelector : '.grid-boxes-in',
-            //isAnimated: true,
             percentPosition: true,
             gutter: '.grid-gutter',
             columnWidth: '.grid-sizer'
-          
-          //function( containerWidth ) {
-                //var box_width = (((containerWidth - 2*gutter)/3) | 0) ;
-                // (1000 - 60)/3 = 940/3 = 313
-                // (x-60)/3 = 300
-                // x-60 = 300*3 = 900
-                // x = 960
-
-                //if (box_width < min_width) {
-                    //box_width = (((containerWidth - gutter)/2) | 0);
-                    // 960-30 /2 = 960/2 = 480
-                //}
-
-                //if (box_width < min_width) {
-                    //box_width = containerWidth;
-                //}
-
-                //$('.grid-boxes-in').width(box_width);
-
-                //return box_width;
-              //}
         });
     });
 });
