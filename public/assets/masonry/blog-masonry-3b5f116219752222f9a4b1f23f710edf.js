68518dae14b21b6cec520c26fac2b7fba26da329
Christoph Geypen <geypenchristoph@gmail.com>
2015-11-11 13:26:54 +0100

    before pushing to heroku

---
+++ public/assets/masonry/blog-masonry-3b5f116219752222f9a4b1f23f710edf.js
@@ -0,0 +1,17 @@
+/**
+ * Base js functions
+ */
+
+
+$(document).ready(function(){
+    var $container = $('.grid-boxes');
+
+    $container.imagesLoaded( function(){
+        $container.masonry({
+            itemSelector : '.grid-boxes-in',
+            percentPosition: true,
+            gutter: '.grid-gutter',
+            columnWidth: '.grid-sizer'
+        });
+    });
+});
