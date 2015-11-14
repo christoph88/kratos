267fee33a9030c8a47705f0744739690e5c35a8c
Christoph Geypen <geypenchristoph@gmail.com>
2015-11-14 20:16:24 +0100

    added feature test for searching

---
+++ test/features/can_search_test.rb
@@ -0,0 +1,31 @@
+require "test_helper"
+
+feature "CanSearch" do
+  scenario "can search for contests" do
+    # do a failed search
+    visit root_path
+
+    within ".navbar-nav" do
+      click_link "leaderboards"
+    end
+
+    within ".input-group" do
+      fill_in "search", with: "testsearch"
+      find(".btn-u").click
+    end
+
+    page.must_have_content "No leaderboards found,"
+
+    # do a working search
+    visit contests_path
+
+    within ".input-group" do
+      fill_in "search", with: "forcontestx"
+      find(".btn-u").click
+    end
+
+    save_and_open_page
+    page.must_have_content "searchforcontestx"
+    #save_and_open_page
+  end
+end
